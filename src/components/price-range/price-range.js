1e79e2daf4c982f7e0523ec4244ddd3c57e38a32
Pashua85 <pavellashuk85@gmail.com>
2019-08-27 11:53:10 +0500

    made search page responsive

--- src/components/price-range/price-range.js
+++ src/components/price-range/price-range.js
@@ -2,7 +2,7 @@ $(function () {
   $('#price-range-slider').slider({
     range: true,
     min: 1000,
-    max: 15400,
+    max: 14900,
     values: [5000, 10000],
     slide: function(event, ui) {
       new Intl.NumberFormat('ru-RU').format($('#price-range-slider').slider('values', 0))
