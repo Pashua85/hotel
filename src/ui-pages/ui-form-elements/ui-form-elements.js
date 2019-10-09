2e7551da4aca10b31de75f25e4861adceedd1d76
Pashua85 <pavellashuk85@gmail.com>
2019-10-09 17:02:09 +0500

    added masked text field to ui-kit

--- src/ui-pages/ui-form-elements/ui-form-elements.js
+++ src/ui-pages/ui-form-elements/ui-form-elements.js
@@ -1,6 +1,7 @@
 $(function() {
-  $('#new-user-birthday').inputmask({
+  $('#ui-masked-input').inputmask({
     'mask': '99.99.9999', 
     'placeholder': 'ДД.ММ.ГГГГ'
   });
+  console.log('he')
 });
